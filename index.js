7cac22f4330e991ad3456ac3e8ad5781e0dd0938
elsot2 <badboys123@hotmail.com>
2016-03-19 16:25:03 +0200

    Added files via upload

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || ' mongodb://<elandis>:<el1183el>@ds013569.mlab.com:13569/heroku_60nnrvl0',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '57czINTvEqIoaN9IgCTaiuTjRGOFsOy0pzZ4VNpl',
+  masterKey: process.env.MASTER_KEY || '8qh6zhuo4HrzNVM2iRlCSG8GBoSmZI2uB4mwxBsL', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://lefkasparseapp.herokuapp.com/parse'  // Don't forget to change to https if needed
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
